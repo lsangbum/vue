b9767913f0b5bdbade491b5c157c7c140ab49f9e
leesangbum <dltkdqja101@gmail.com>
2022-02-22 08:37:36 +0900

    tobyspring MethodInterceptor 활용

---
+++ demo/src/main/java/com/example/demo/proxy/Hello.java
@@ -0,0 +1,11 @@
+package com.example.demo.proxy;
+
+public interface Hello {
+
+    String sayHello(String name);
+
+    String sayHi(String name);
+
+    String sayBye(String name);
+
+}//interface
